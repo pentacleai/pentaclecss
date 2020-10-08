727ce8fac8da4d9715af745b9eee541842ffafa4
charlottefranklin <charlotte@cfranklin.com>
2020-10-08 13:31:07 +1300

    CF - move layout page

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -5,9 +5,9 @@ class Navigation extends Component {
     render() {
         return <nav>
             <ul className={"nav-content"}>
+                <li><Link to="/layout">Layout</Link></li>
                 <li><Link to="/box">Box</Link></li>
                 <li><Link to="/flex">Flex</Link></li>
-                <li><Link to="/layout">Layout</Link></li>
                 <li><Link to="/fonts">Fonts</Link></li>
                 <li><Link to="/typography">Typography</Link></li>
             </ul>
