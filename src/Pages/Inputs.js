8501bfc1575ffabd4fd97e118ccfd7a7975fceb9
Charlotte Franklin <charlotte@cfranklin.com>
2021-02-01 18:34:47 +1300

    Inputs

--- src/Pages/Inputs.js
+++ src/Pages/Inputs.js
@@ -12,12 +12,55 @@ class Inputs extends Component {
                 <button className={"secondary"}>Secondary button</button>
                 <button className={"cancel"}>Cancel button</button>
                 <button className={"delete"}>Delete button</button>
-                <Gist gist="0993d5c23318d2aa6bd9e2f1a32d3765" />
+                <Gist gist="0993d5c23318d2aa6bd9e2f1a32d3765"/>
             </article>
             <article>
                 <h2>Checkbox</h2>
                 <input type="checkbox"/>
-                <Gist gist="ae3f25050f0d04bfef0ca2db8b7f30dc" />
+                <Gist gist="ae3f25050f0d04bfef0ca2db8b7f30dc"/>
+            </article>
+            <article>
+                <h2>Date</h2>
+                <label htmlFor="birthday">Date</label>
+                <input type="date" id="birthday" name="birthday"
+                       value="2018-07-22"
+                       min="2018-01-01" max="2018-12-31"/>
+                <Gist gist="c901d39a498e089cc97d8aa6f9384804"/>
+            </article>
+            <article>
+                <h2>File</h2>
+                <label htmlFor="avatar">Upload avatar</label>
+                <input type="file"
+                       id="avatar" name="avatar"
+                       accept="image/png, image/jpeg"/>
+                <Gist gist="7ae1efd939001d477ec4558453fde49a"/>
+            </article>
+            <article>
+                <h2>Radio</h2>
+                <fieldset>
+                    <legend>Favourite pet</legend>
+                    <input type="radio" id="cat" name="pet" value="cat"/><label htmlFor="cat">Cat</label>
+                    <input type="radio" id="cat" name="pet" value="dog"/><label htmlFor="dog">Dog</label>
+                    <input type="radio" id="cat" name="pet" value="rabbit"/><label htmlFor="rabbit">Rabbit</label>
+                </fieldset>
+                <Gist gist="d5911d3362321f8e1c01503cacbac052"/>
+            </article>
+            <article>
+                <h2>Select</h2>
+                <label htmlFor="pet-select">Favourite pet</label>
+                <select name="pets" id="pet-select">
+                    <option value="">Choose a pet</option>
+                    <option value="dog">Dog</option>
+                    <option value="cat">Cat</option>
+                    <option value="rabbit">Rabbit</option>
+                </select>
+                <Gist gist="6687cc0e411a1788fc98959e7424d4f7"/>
+            </article>
+            <article>
+                <h2>Text</h2>
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name" name="name" required/>
+                <Gist gist="f559f698e7660548a3f4c5c535ac7650"/>
             </article>
         </Layout>;
     }
