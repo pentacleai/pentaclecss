39be29c7502b304b7656c28c7fd8c4e2d35cd344
Charlotte Franklin <charlotte@cfranklin.com>
2021-02-02 16:38:09 +1300

    Inputs

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,11 +6,11 @@ class Navigation extends Component {
         return <nav>
             <ul className={"nav-content"}>
                 <li><Link to="/layout">Layout</Link></li>
-                <li><Link to="/inputs">Inputs</Link></li>
                 <li><Link to="/box">Box</Link></li>
                 <li><Link to="/flex">Flex</Link></li>
                 <li><Link to="/fonts">Fonts</Link></li>
                 <li><Link to="/typography">Typography</Link></li>
+                <li><Link to="/inputs">Inputs (WIP)</Link></li>
             </ul>
         </nav>
     }
