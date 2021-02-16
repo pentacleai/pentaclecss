853350500dc15109fe5dbb1776c71f62a204e840
charlottefranklin <charlotte@cfranklin.com>
2021-02-16 16:35:59 +1300

    CF - about page

--- src/App.js
+++ src/App.js
@@ -29,18 +29,29 @@ class App extends Component {
 function Main() {
     return <>
         <Layout>
-            <h1>Hello</h1>
-            <article>
-                <p>Pentacle CSS is super simple css library written in sass.</p>
-                <p>It's designed to provide a clean, lightweight layout that just works.</p>
-                <p>Get the <a href={"https://www.npmjs.com/package/pentacle-css"} target={"_blank"} rel="noopener noreferrer">npm</a></p>
-            </article>
-            <article>
-                <h2>Roadmap</h2>
-                <p>
-                   View at <a href={"https://trello.com/b/3ZPKFldK/pentacle-css"} target={"_blank"} rel="noopener noreferrer">trello</a>
-                </p>
-            </article>
+            <section class={"editorial-content"}>
+                <h1>Hello</h1>
+                <article>
+                    <p>Pentacle CSS is super simple css library written in sass.</p>
+                    <p>It's designed to provide a clean, lightweight layout that just works.</p>
+                </article>
+                <article>
+                    <h2>About</h2>
+                    <p>By day I'm a consultant specialising in refactoring bootstrap and other crufty ui libs out of
+                        enterprise software systems at banks and FTSE100 companies, and putting in custom sass libraries.</p>
+                    <p>A reduction in often unnecessary design complexity gives large rewards in ease of use, accessibility and performance.</p>
+                    <p>This incarnation of the library is a new work in progress, however none of the selector names will change as standard html element naming conventions are used.</p>
+                    <p>I am currently writing sass functions and working on theming so people can change overall sizing and colour themes by switching just a few sets of variables.</p>
+                </article>
+                <article className={"margin-top-2"}>
+                    <p>
+                        View the <a href={"https://trello.com/b/3ZPKFldK/pentacle-css"} target={"_blank"}
+                                   rel="noopener noreferrer">trello</a> board
+                    </p>
+                    <p>Get the <a href={"https://www.npmjs.com/package/pentacle-css"} target={"_blank"}
+                                  rel="noopener noreferrer">npm</a></p>
+                </article>
+            </section>
         </Layout>
     </>
 }
