5da96432eaebf5663bff2ecd286ba34ea1a655ef
charlottefranklin <charlotte@cfranklin.com>
2021-02-18 00:06:44 +1300

    CF - copy

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function Main() {
                     <h2>About</h2>
                     <p>A reduction in design complexity give rewards in ease of use,
                         accessibility and performance – for developers and people who use the software we create.</p>
-                    <p>I write away bootstrap and third-party ui plugins from enterprise software at banks and FTSE100 companies, and put in custom sass libraries
+                    <p>I write away bootstrap and third-party ui plugins from enterprise software at banks and FTSE100 companies, and replace them with clean sass libraries
                         and hand-rolled js components.</p>
                 </article>
                 <article>
