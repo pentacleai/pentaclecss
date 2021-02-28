e51a78a5fa4058f30963da0b4d12f9d3581d2854
charlottefranklin <charlotte@cfranklin.com>
2021-03-01 02:14:10 +1300

    CF- fix neon

---
+++ build/precache-manifest.635ef4c13202040ea44039560af4a1a2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "768c18dd245143322de848727b7d06b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939dceae4dc523c1c1aa",
+    "url": "/static/css/main.5e5d7381.chunk.css"
+  },
+  {
+    "revision": "e89fb68e679add12981b",
+    "url": "/static/js/2.beefb0e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.beefb0e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "939dceae4dc523c1c1aa",
+    "url": "/static/js/main.bb83201d.chunk.js"
+  },
+  {
+    "revision": "8cf09661d09c1e176389",
+    "url": "/static/js/runtime-main.2a776348.js"
+  },
+  {
+    "revision": "4fca5874504b7e1ef937f138b96a137e",
+    "url": "/static/media/FiraCode-Bold.4fca5874.ttf"
+  },
+  {
+    "revision": "16cfbc94a37eee37fc38930a0ee6d3d1",
+    "url": "/static/media/FiraCode-Light.16cfbc94.ttf"
+  },
+  {
+    "revision": "199a7bd6ec05cc0838b1cffa21158b14",
+    "url": "/static/media/FiraCode-Medium.199a7bd6.ttf"
+  },
+  {
+    "revision": "dc34dd991980e8155dd1f146b0f7cd6d",
+    "url": "/static/media/FiraCode-Regular.dc34dd99.ttf"
+  },
+  {
+    "revision": "d98c7b35a8b2ea0cc7bbccd7468ea404",
+    "url": "/static/media/FiraCode-SemiBold.d98c7b35.ttf"
+  },
+  {
+    "revision": "8ef52a15e44481b41e7db3c7eaf9bb83",
+    "url": "/static/media/material.8ef52a15.ttf"
+  }
+]);
